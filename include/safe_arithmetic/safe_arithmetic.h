cec05d4be834c2ad41f16e44a1f0bbd77f3dc443
Donghyeon Kim <donghyeon@tacocube.com>
2017-09-05 12:08:46 -0400

    Modify Header Guards to Ommit Initial Directory Name
    Leading "SAFE_ARITHMETIC_" phrase is unnecessary.

--- include/safe_arithmetic/safe_arithmetic.h
+++ include/safe_arithmetic/safe_arithmetic.h
@@ -1,4 +1,4 @@
-#ifndef SAFE_ARITHMETIC_SAFE_ARITHMETIC_H
-#define SAFE_ARITHMETIC_SAFE_ARITHMETIC_H
+#ifndef SAFE_ARITHMETIC_H
+#define SAFE_ARITHMETIC_H
 #include <safe_arithmetic/safe_arithmetic_int.h>
-#endif //SAFE_ARITHMETIC_SAFE_ARITHMETIC_H
+#endif //SAFE_ARITHMETIC_H
